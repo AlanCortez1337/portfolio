58e6d1920c5d3ac5a08d7bf4a0b898719d918a5f
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2022-05-14 13:59:42 -0700

    almost fixed background

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,32 @@ function App() {
     <>
       <section className="hero">
         <div className="blurp-hero">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, hic error? Perferendis id, eveniet ratione amet omnis delectus error quos aliquid blanditiis maxime deleniti adipisci odit. Nihil necessitatibus nostrum animi?
+          <h3>hello i am alan cortez,</h3>
+          <h1>an inspiring full stack developer_</h1>
+          <button>resume</button>
         </div>
         <Wave shape={1}></Wave>
         <Wave shape={2}></Wave>
       </section>
       <section className="about-me">
         <div className="blurp-abt">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, atque?
+          <h3>hello i am alan cortez,</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, sit!</p>
         </div>
         <Wave shape={3}></Wave>
       </section>
-      {/* <section className="projects">
-
+      <section className="projects">
+        <div className="blurp-abt">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, atque?
+        </div>
+        <Wave shape={4}></Wave>
       </section>
-      <section className="contact">
-
+      {/* <section className="contact">
+        <div className="blurp-abt">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, atque?
+        </div>
+        <Wave shape={5}></Wave>
+        <Wave shape={6}></Wave>
       </section> */}
     </>
   )
--- src/components/svg/wave1.jsx
+++ src/components/svg/wave1.jsx
@@ -16,26 +16,26 @@ function Shapes({shape}) {
                 }
                 {/* Fix width/height */}
                 {shape === 3 && 
-                    <svg className="wave-3-svg" viewBox="0 0 1512 910" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path className="wave-3" d="M0 35.3952C0 35.3952 314 -32.8825 378 20.2223C442 73.3272 665 51.6517 701.5 122.639C738 193.626 673.929 712.962 884 738C1094.07 763.038 1548.22 867.034 1515 910H0V35.3952Z" fill="#464D77"/>
+                    <svg className="wave-3-svg" viewBox="0 0 1512 843" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1.19209e-05 216C1.19209e-05 216 -97.5001 -150.5 631 73.5001C1359.5 297.5 1512 216 1512 216V842.999H1.19209e-05V216Z" fill="#464D77"/>
                     </svg>
                 }
-                {/* {shape === 4 && 
-                    <svg width="1512" height="783" viewBox="0 0 1512 783" fill="none" xmlns="http://www.w3.org/2000/svg">
+                {shape === 4 && 
+                    <svg className="wave-4-svg" viewBox="0 0 1512 783" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1.90381e-05 145C1.90381e-05 145 -14.4999 -93.0001 750 41.9999C1514.5 177 1512 145 1512 145V783H1.90381e-05V145Z" fill="#3B617D"/>
                     </svg>
                     
                 }
                 {shape === 5 && 
-                    <svg width="1512" height="719" viewBox="0 0 1512 719" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="wave-5-svg" viewBox="0 0 1512 719" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 71.2992C0 71.2992 268 115.767 556.5 89.7151C845 63.6634 978 35.3657 1192 5.72061C1406 -23.9245 1512 71.2992 1512 71.2992V719H0V71.2992Z" fill="#248889"/>
                     </svg>
                 }
                 {shape === 6 && 
-                    <svg width="1512" height="207" viewBox="0 0 1512 207" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="wave-6-svg" viewBox="0 0 1512 207" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M2.78652e-05 57C2.78652e-05 57 -18.5 -32.5 233 13C484.5 58.5 1512 57 1512 57V207H2.78652e-05V57Z" fill="#02C39A"/>
                     </svg>
-                } */}
+                }
             </>
     );
 }
