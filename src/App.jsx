384ba8b8c738a44504c9b1e94209487d59176c60
Alan Cortez <acortez1337@csu.fullerton.edu>
2022-05-19 21:54:23 -0700

    worked on abt section with the grid, not fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import Wave from "./components/svg/wave1"
+import Wave from "./components/svg/wave"
 import './App.css'
+import MeBadge from "/assests/meBadge.png"
+import DndBadge from "/assests/dnd.png"
+import LuaBadge from "/assests/lua.png"
+import BreadBadge from "/assests/bread.png"
 
 function App() {
 
@@ -15,18 +19,31 @@ function App() {
         <Wave shape={2}></Wave>
       </section>
       <section className="about-me">
-        <div className="blurp-abt">
-          <h3>hello i am alan cortez,</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, sit!</p>
+        <div className="abt-wrapper">
+          <div className="blurp-abt">
+            <h3>hello i am alan cortez,</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, sit!</p>
+          </div>
+          <div className="abt-grid">
+              {/* <p>a</p>
+              <p>b</p>
+              <p>c</p>
+              <p>d</p> */}
+
+              <img src={MeBadge} alt="me" className="badge" />
+              <img src={LuaBadge} alt="me" className="badge" />
+              <img src={BreadBadge} alt="me" className="badge" />
+              <img src={DndBadge} alt="me" className="badge" />
+          </div>
         </div>
         <Wave shape={3}></Wave>
       </section>
-      <section className="projects">
+      {/* <section className="projects">
         <div className="blurp-abt">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, atque?
         </div>
         <Wave shape={4}></Wave>
-      </section>
+      </section> */}
       {/* <section className="contact">
         <div className="blurp-abt">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, atque?
