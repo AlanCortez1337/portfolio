b9f317fd2c2108b153bfdf810244522ac3e58d33
Alan Cortez <acortez1337@csu.fullerton.edu>
2022-05-20 21:39:00 -0700

    finished first iteration of badges, will change animations to using FramerMotion next

--- src/App.jsx
+++ src/App.jsx
@@ -22,24 +22,39 @@ function App() {
         {/* <div className="abt-wrapper"> */}
           <div className="abt-grid">
             <div className="blurp-abt">
-              <h3>hello i am alan cortez,</h3>
+              <h3>About Me</h3>
+              <p>
+                I am a simple man with a modest goal, to make people's lives easier. Currently attending CSUF with a B.S. in Computer Science wrapping up in May 2024. I love coding, baking bread, playing D&D, but most importantly helping you.
+              </p>
             </div>
-            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, sit!</p> */}
-
-            <img src={MeBadge} alt="me" className="badge" width={300} height={300} />
-            <img src={LuaBadge} alt="me" className="badge" width={250} height={250} />
-            <img src={BreadBadge} alt="me" className="badge" width={275} height={275} />
+            <img src={MeBadge} alt="me" className="badge" width={250} height={250} />
+            <img src={LuaBadge} alt="me" className="badge" width={175} height={175} />
+            <img src={BreadBadge} alt="me" className="badge" width={225} height={225} />
             <img src={DndBadge} alt="me" className="badge" width={150} height={150} />
           </div>
         {/* </div> */}
         <Wave shape={3}></Wave>
       </section>
-      {/* <section className="projects">
-        <div className="blurp-abt">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, atque?
+      <section className="projects">
+        <div className="project-grid">
+          <div className="proj1">
+            asdf
+          </div>
+          <div className="proj2">
+            asdffff
+          </div>
+          <div className="proj3">
+            asdfsaf
+          </div>
+          <div className="proj4">
+            asdfasdf
+          </div>
+          <div className="proj5">
+            asdfsdf
+          </div>
         </div>
-        <Wave shape={4}></Wave>
-      </section> */}
+        {/* <Wave shape={4}></Wave> */}
+      </section>
       {/* <section className="contact">
         <div className="blurp-abt">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, atque?
