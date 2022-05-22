efbcb1caa0a06de14362761ca6b1ebd9d99936cc
Alan Cortez <acortez1337@csu.fullerton.edu>
2022-05-21 22:12:32 -0700

    added animations to rotate, has jumping bug

--- src/components/images/badge.jsx
+++ src/components/images/badge.jsx
@@ -12,28 +12,40 @@ function BadgeImage({badgeName}) {
         <>
             {badgeName === "me" && 
                 <motion.img
-                    whileHover={{duration: 0.4, rotate: 360}}
+                    // drag
+                    // dragSnapToOrigin
+                    // ERROR IT JUMPS LUL
+                    initial={{ rotate: 45 }}
                     animate={{ rotate: 360 }}
+                    exit={{ rotate: 45}}
+                    whileHover={{transition: {type: "spring", velocity: 4}, rotate: -315}}
+                    
                     transition={{ repeat: Infinity, duration: 8, ease: "linear" }}
                     
-                    src={MeBadge} alt="me" className="badge" width={250} height={250} 
+                    src={MeBadge} alt="me" className="badge badge1" width={250} height={250} 
                 />
             }
             {badgeName === "lua" && 
                 <motion.img 
-
+                    whileHover={{transition: {type: "spring", velocity: 4}, rotate: -360}}
+                    animate={{ rotate: 360 }}
+                    transition={{ repeat: Infinity, duration: 8, ease: "linear" }}
                     src={LuaBadge} alt="me" className="badge" width={175} height={175} 
                 />
             }
             {badgeName === "bread" && 
                 <motion.img 
-
+                    whileHover={{transition: {type: "spring", velocity: 4}, rotate: -360}}
+                    animate={{ rotate: 360 }}
+                    transition={{ repeat: Infinity, duration: 8, ease: "linear" }}
                     src={BreadBadge} alt="me" className="badge" width={225} height={225} 
                 />
             }
             {badgeName === "dnd" && 
                 <motion.img 
-
+                    whileHover={{transition: {type: "spring", velocity: 4}, rotate: -315}}
+                    animate={{ rotate: 360 }}
+                    transition={{ repeat: Infinity, duration: 8, ease: "linear" }}
                     src={DndBadge} alt="me" className="badge" width={150} height={150} 
                 />
             }            
