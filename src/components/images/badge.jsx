2a77a9aaffe2a4e8995406acff2e76228114d942
Alan Cortez <acortez1337@csu.fullerton.edu>
2022-05-22 22:10:38 -0700

    finished animation for badges, fix registration bug on top two badges tho

--- src/components/images/badge.jsx
+++ src/components/images/badge.jsx
@@ -5,13 +5,28 @@ import BreadBadge from "/assests/bread.png"
 import { motion } from "framer-motion"
 
 function BadgeImage({badgeName}) {
+    const badgeRotation = {
+        badge1: {
+            rotate: 360, 
+            y: '-6vh'
+        },
+        badge2: {
+            rotate: 360, 
+            y: '3vh'
+        },
+    }
+    
 
     const badgeBounce = {
         y: {
-            yoyo: Infinity, duration: 1, ease: "linear"
+            yoyo: Infinity, 
+            duration: 1, 
+            ease: "easeInOut"
         },
         rotate: {
-            repeat: Infinity, duration: 10, ease: "linear"
+            repeat: Infinity, 
+            duration: 10, 
+            ease: "linear"
         }
     }
 
@@ -19,34 +34,35 @@ function BadgeImage({badgeName}) {
         <>
             {badgeName === "me" && 
                 <motion.img
-                    // initial={{ rotate: -45 }}
-                    animate={{ rotate: 405, y: ['-75px', '-125px'] }}
+                    animate={badgeRotation.badge1}
                     transition={badgeBounce}
-                    whileHover={{transition: {type: "spring", velocity: 4}, rotate: -315, y: ['0px', '0px']}}
-                    src={MeBadge} alt="me" className="badge badge1" width={250} height={250} 
+                    whileHover={{transition: {type: "spring", velocity: 6}, rotate: -315, y: '-8vh'}}
+                    onHoverStart={() => {badgeRotation.badge1.rotate = 406}}
+                    src={MeBadge} alt="me" className="badge badge1" width={225} height={225} 
                 />
             }
             {badgeName === "lua" && 
                 <motion.img 
-                    whileHover={{transition: {type: "spring", velocity: 4}, rotate: -360}}
-                    animate={{ rotate: 360 }}
-                    transition={{ repeat: Infinity, duration: 8, ease: "linear" }}
+                    animate={{ rotate: -360, y: '-3vh' }}
+                    transition={badgeBounce}
+                    whileHover={{transition: {type: "spring", velocity: 4}, rotate: 360, y: '-6vh'}}
                     src={LuaBadge} alt="me" className="badge" width={175} height={175} 
                 />
             }
             {badgeName === "bread" && 
                 <motion.img 
-                    whileHover={{transition: {type: "spring", velocity: 4}, rotate: -360}}
-                    animate={{ rotate: 360 }}
-                    transition={{ repeat: Infinity, duration: 8, ease: "linear" }}
+                    animate={{ rotate: -360, y: '3vh' }}
+                    transition={badgeBounce}
+                    whileHover={{transition: {type: "spring", velocity: 1}, rotate: 360, y: '0vh'}}
                     src={BreadBadge} alt="me" className="badge" width={225} height={225} 
                 />
             }
             {badgeName === "dnd" && 
                 <motion.img 
-                    whileHover={{transition: {type: "spring", velocity: 4}, rotate: -315}}
-                    animate={{ rotate: 360 }}
-                    transition={{ repeat: Infinity, duration: 8, ease: "linear" }}
+                    animate={badgeRotation.badge2}
+                    transition={badgeBounce}
+                    whileHover={{transition: {type: "spring", velocity: 4}, rotate: -315, y: '1vh'}}
+                    onHoverStart={() => {badgeRotation.badge2.rotate = 406}}
                     src={DndBadge} alt="me" className="badge" width={150} height={150} 
                 />
             }            
