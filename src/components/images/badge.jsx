f631da3ec416f2ed11b49aebff383d31d7ffff18
Alan Cortez <acortez1337@csu.fullerton.edu>
2022-05-24 22:24:00 -0700

    scrap twist idea

--- src/components/images/badge.jsx
+++ src/components/images/badge.jsx
@@ -3,8 +3,11 @@ import DndBadge from "/assests/dnd.png"
 import LuaBadge from "/assests/lua.png"
 import BreadBadge from "/assests/bread.png"
 import { motion } from "framer-motion"
+import { useState } from "react" 
 
 function BadgeImage({badgeName}) {
+    const [speed, setSpeed] = useState(6);
+
     const badgeRotation = {
         badge1: {
             rotate: 360, 
@@ -26,7 +29,7 @@ function BadgeImage({badgeName}) {
         },
         rotate: {
             repeat: Infinity, 
-            duration: 6, 
+            duration: speed, 
             ease: "linear"
         }
     }
@@ -39,45 +42,46 @@ function BadgeImage({badgeName}) {
                     transition={badgeBounce}
                     // since we want to see my face vertical I need to rotate 45 deg from the original point
                     // essentially turns the square into a diamond
-                    whileHover={{transition: {type: "spring", velocity: 6}, rotate: -315, y: '-10vh'}}
+                    whileHover={{transition: {duration: 1}, y: '-10vh'}}
+                    onHoverStart={(()=>{setSpeed(1)})}
                     // now that we set the new origin to -315, we still want to make a full loop around
                     // to do so we just need to make sure the badge is combating the -315 by adding 45 to 360
                     // in order to make it seem as if it is going full circle
                     // onHoverStart={() => {badgeRotation.badge1.rotate = 406}}
-                    onHoverStart={(e) => {
-                        const startPoint = e.target.style.transform.search("rotate") + 7;
-                        const endPoint = e.target.style.transform.search("deg");
+                    // onHoverStart={(e) => {
+                    //     const startPoint = e.target.style.transform.search("rotate") + 7;
+                    //     const endPoint = e.target.style.transform.search("deg");
 
                         
-                        let newStartDegree = parseFloat(e.target.style.transform.substring(startPoint, endPoint));
-                        console.log(e.target.style);
+                    //     let newStartDegree = parseFloat(e.target.style.transform.substring(startPoint, endPoint));
+                    //     console.log(e.target.style);
                         
-                        let rotateDegrees;
-                        // TODO
-                            // fix the comments above
-                            // do the math calculation so that it does a full loop no matter what
-                            // victory lap
-                        // let newLoopAngle = newStartDegree
-                        // console.log(360 + newStartDegree);
-                        if(newStartDegree > 0) {
-                            console.log("positive", newStartDegree);
-                            // 360 = newStartDegree + rotation => 360 - newStartDegree = rotation
-                            // rotation + 360 => rotate
-                            rotateDegrees = newStartDegree;
-                            console.log(rotateDegrees+405);
-                            badgeRotation.badge1.rotate = rotateDegrees;
-                        } else {
-                            console.log("negative", newStartDegree);
+                    //     let rotateDegrees;
+                    //     // TODO
+                    //         // fix the comments above
+                    //         // do the math calculation so that it does a full loop no matter what
+                    //         // victory lap
+                    //     // let newLoopAngle = newStartDegree
+                    //     // console.log(360 + newStartDegree);
+                    //     if(newStartDegree > 0) {
+                    //         console.log("positive", newStartDegree);
+                    //         // 360 = newStartDegree + rotation => 360 - newStartDegree = rotation
+                    //         // rotation + 360 => rotate
+                    //         rotateDegrees = newStartDegree;
+                    //         console.log(rotateDegrees+405);
+                    //         badgeRotation.badge1.rotate = rotateDegrees;
+                    //     } else {
+                    //         console.log("negative", newStartDegree);
                             
-                            rotateDegrees = 360 - (newStartDegree);
-                            // rotateDegrees = rotateDegrees + 360;
-                            console.log(rotateDegrees, "result: ", rotateDegrees);
-                            badgeRotation.badge1.rotate = rotateDegrees;
-                            console.log(badgeRotation.badge1.rotate);
-                        }
+                    //         rotateDegrees = 360 - (newStartDegree);
+                    //         // rotateDegrees = rotateDegrees + 360;
+                    //         console.log(rotateDegrees, "result: ", rotateDegrees);
+                    //         badgeRotation.badge1.rotate = rotateDegrees;
+                    //         console.log(badgeRotation.badge1.rotate);
+                    //     }
 
-                        // badgeRotation.badge1.rotate = (360 + newStartDegree);
-                    }}
+                    //     // badgeRotation.badge1.rotate = (360 + newStartDegree);
+                    // }}
                     src={MeBadge} alt="me" className="badge badge1" width={225} height={225} 
                 />
             }
