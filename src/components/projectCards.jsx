a7d4e9e6b9848a5174ae219e22226017a007af6c
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2022-06-24 12:06:51 -0700

    refractored how the gif images are uploaded, should now be lazy loaded

--- src/components/projectCards.jsx
+++ src/components/projectCards.jsx
@@ -2,6 +2,10 @@ import { IoGameController, IoLogoGithub } from "react-icons/io5";
 import { SiTinder, SiDiscord } from "react-icons/si";
 import { useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
+import DiscordBotDemo from "/assests/DiscordBotDemo.webp"
+import MemoryGameDemo from "/assests/MemoryGameDemo.webp"
+import FoodTinderDemo from "/assests/FoodTinderDemo.webp"
+
 
 function ProjCard({type}) {
     const [isVisible, setIsVisible] = useState(true);
@@ -12,6 +16,8 @@ function ProjCard({type}) {
             title: " Bot",
             blurp: "My goal with this passion project was to annoy my friends to the maximum. My GitHub repository shows all the different features I have implemented.",
             link: "https://github.com/AlanCortez1337/Discord-Bot-Python",
+            gif: DiscordBotDemo,
+            imgAltName: "discord bot demo project",
             yBoundry: "-20px",
             rotate: "3deg"
         },
@@ -19,6 +25,9 @@ function ProjCard({type}) {
         {
             title: "Memory ",
             blurp: "This is my first finished React project which is a memory card game with funny animals. Additional information about this project is found in my GitHub repository.",
+            link: "https://github.com/AlanCortez1337/Memory-Mania",
+            gif: MemoryGameDemo,
+            imgAltName: "memory game demo project",
             yBoundry: "40px",
             rotate: "-2deg"
         },
@@ -27,6 +36,8 @@ function ProjCard({type}) {
             title: "Food ",
             blurp: "In collaboration with my ACM club, we developed a Tinder clone for food. My contributions were the \"create post\" page, nav bar, and assisting in the login screen of Food Tinder.",
             link: "https://github.com/acmCSUFDev/Food-Tinder",
+            gif: FoodTinderDemo,
+            imgAltName: "food tinder demo project",
             yBoundry: "20px",
             rotate: "2deg"
         }
@@ -58,6 +69,7 @@ function ProjCard({type}) {
                 onClick={changeVisibility}
                 className={`project-card  bg-${type}`}
             >
+                <img src={projects[type].gif} alt={projects[type].imgAltName} loading="lazy" className="bg-Image"/>
                 <AnimatePresence>
                     {isVisible ? 
                         <motion.h3
